141302308ed6ad7b457bae0e7fc760e4da54fe19
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-02-29 13:58:41 +0100

    using new git based versioning...

--- src/main/scala/eu/nomad_lab/parsers/GpawParser.scala
+++ src/main/scala/eu/nomad_lab/parsers/GpawParser.scala
@@ -1,15 +1,23 @@
 package eu.nomad_lab.parsers
-import eu.nomad_lab.DefaultPythonInterpreter
+import eu.{nomad_lab => lab}
 import org.{json4s => jn}
+import scala.collection.breakOut
 
 object GpawParser extends SimpleExternalParserGenerator(
   name = "GpawParser",
   parserInfo = jn.JObject(
     ("name" -> jn.JString("GpawParser")) ::
-      ("version" -> jn.JString("1.0")) :: Nil),
+      ("parserId" -> jn.JString("GpawParser" + lab.GpawVersionInfo.version)) ::
+      ("versionInfo" -> jn.JObject(
+        ("nomadCoreVersion" -> jn.JString(lab.NomadCoreVersionInfo.version)) ::
+          (lab.GpawVersionInfo.toMap.map{ case (key, value) =>
+            (key -> jn.JString(value.toString))
+          }(breakOut): List[(String, jn.JString)])
+      )) :: Nil
+  ),
   mainFileTypes = Seq("application/x-gtar"),
   mainFileRe = "".r,
-  cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/gpaw/parser/parser-gpaw/parser.py",
+  cmd = Seq(lab.DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/gpaw/parser/parser-gpaw/parser.py",
     "${mainFilePath}"),
   resList = Seq(
     "parser-gpaw/parser.py",
@@ -18,11 +26,11 @@ object GpawParser extends SimpleExternalParserGenerator(
     "nomad_meta_info/common.nomadmetainfo.json",
     "nomad_meta_info/meta_types.nomadmetainfo.json",
     "nomad_meta_info/gpaw.nomadmetainfo.json"
-  ) ++ DefaultPythonInterpreter.commonFiles(),
+  ) ++ lab.DefaultPythonInterpreter.commonFiles(),
   dirMap = Map(
     "parser-gpaw" -> "parsers/gpaw/parser/parser-gpaw",
     "nomad_meta_info" -> "nomad-meta-info/meta_info/nomad_meta_info"
-  ) ++ DefaultPythonInterpreter.commonDirMapping()
+  ) ++ lab.DefaultPythonInterpreter.commonDirMapping()
 ) {
   override def isMainFile(filePath: String, bytePrefix: Array[Byte], stringPrefix: Option[String]): Option[ParserMatch] = {
    if (filePath.endsWith(".gpw"))
